9a33c20768bcea986e9b5714f42e1d5e4abc4379
hhiroki <gyouzakuuka@gmail.com>
2021-05-08 14:52:50 +0900

    update

---
+++ examples/simple-table-range.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+
+use HerSQL::btree::{BTree, SearchMode};
+use HerSQL::buffer::{BufferPool, BufferPoolManager};
+use HerSQL::disk::{DiskManager, PageId};
+use HerSQL::tuple;
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("simple.her")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = BufferPoolManager::new(disk, pool);
+
+    let btree = BTree::new(PageId(0));
+    let mut search_key = vec![];
+    tuple::encode([b"y"].iter(), &mut search_key); // バイトにエンコードしてから入れてね
+    let mut iter = btree.search(&mut bufmgr, SearchMode::Key(search_key))?;
+
+    while let Some((key, value)) = iter.next(&mut bufmgr)? {
+        let mut record = vec![];
+        tuple::decode(&key, &mut record);
+        tuple::decode(&value, &mut record);
+        println!("{:?}", tuple::Pretty(&record));
+    }
+    Ok(())
+}
